1c2fc4da686429c354e1baaba2a81402a036e28c
Youssef Elzedy <youssefelzedy90@gmail.com>
2023-09-20 02:48:18 +0300

    fix task 3

--- choseOrder.c
+++ choseOrder.c
@@ -4,11 +4,11 @@ int choseOrder(char **strRead_cp, char **argv,
 unsigned int numCount)
 {
 	int stat;
-	/*static int error = 1;*/
+	static int error = 0;
 
 	if (_strcmp(strRead_cp[0], "exit") == 0)
 	{
-		if (modify_exit(strRead_cp, argv[0], errno, numCount) == 2
+		if (modify_exit(strRead_cp, argv[0], error, numCount) == 2
 			&& !isatty(0))
 			exit(2);
 	}
@@ -20,18 +20,18 @@ unsigned int numCount)
 	else if (_strcmp(strRead_cp[0], "cd") == 0)
 	{
 		stat = getCdMod(strRead_cp, argv[0], numCount);
-		/*error = errno;*/
+		error = errno;
 		/*free_2d(strRead_cp);*/
 	}
 	else
 	{
 		stat = exeFile(strRead_cp[0], strRead_cp, argv[0], numCount);
-		/*error = errno;*/
+		error = errno;
 		/*printf("error: %d - stat: %d\n", error, stat);*/
 		if (stat == -1)
 			freeExit(strRead_cp, 1);
 		else if (stat == 127 && !isatty(0))
 			freeExit(strRead_cp, 127);
 	}
-	return (errno);
+	return (error);
 }
